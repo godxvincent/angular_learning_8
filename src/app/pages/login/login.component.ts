d379c3ef6afb1e355874c686c7a075311821bae1
godxvincent <godxvincent@gmail.com>
2020-12-13 17:36:49 -0500

    :sparkles: Sing Up and Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from 'src/app/models/usuario.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor() { }
+  constructor( private authService: AuthService) { }
 
   ngOnInit(): void {
     this.usuario = new UsuarioModel();
@@ -24,6 +25,11 @@ export class LoginComponent implements OnInit {
     console.log('formulario enviado');
     console.log(this.usuario);
     console.log(formulario);
+    this.authService.login(this.usuario).subscribe( respuestaFireBase => {
+      console.log(respuestaFireBase);
+    }, (err) => {
+      console.log(err.error.error.message);
+    });
  }
 
 }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -11,7 +12,7 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor() { }
+  constructor( private authService: AuthService) { }
 
   ngOnInit(): void {
     this.usuario = new UsuarioModel();
@@ -24,6 +25,11 @@ export class RegistroComponent implements OnInit {
       console.log('formulario enviado');
       console.log(this.usuario);
       console.log(formulario);
+      this.authService.nuevoUsuario(this.usuario).subscribe( respuestaFireBase => {
+        console.log(respuestaFireBase);
+      }, (err) => {
+        console.log(err.error.error.message);
+      });
    }
 
 }
